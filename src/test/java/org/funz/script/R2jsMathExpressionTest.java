b1848948c4e274edb10cf587f6f47a286c8920c0
yannrichet <richetyann@gmail.com>
2020-09-14 18:13:40 +0200

    back to delta<-gamma

--- src/test/java/org/funz/script/R2jsMathExpressionTest.java
+++ src/test/java/org/funz/script/R2jsMathExpressionTest.java
@@ -88,7 +88,7 @@ public class R2jsMathExpressionTest {
                 + "}");
 
         assert engine.R.ls("f") != null : "Cannot eval f";
-        engine.R.voidEval("options = list(nmax = 10, gamma = 1, epsilon = 0.01, target=0)");
+        engine.R.voidEval("options = list(nmax = 10, delta = 1, epsilon = 0.01, target=0)");
         assert engine.R.print("options") != null : "Cannot eval options";
 
         assert engine.R.voidEval("gd = GradientDescent(options)") : "Failed last voidEval: " + engine.R.notebook();
