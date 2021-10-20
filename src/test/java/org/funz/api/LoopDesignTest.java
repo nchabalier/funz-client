3f6bb199e5999e4287ea0576d4024f781768c921
yannrichet <richetyann@gmail.com>
2021-10-20 13:21:40 +0200

    fix name of min tag

--- src/test/java/org/funz/api/LoopDesignTest.java
+++ src/test/java/org/funz/api/LoopDesignTest.java
@@ -183,7 +183,7 @@ public class LoopDesignTest extends org.funz.api.TestUtils {
         }
         System.err.println(loopDesign.getResults().keySet());
         System.err.println(loopDesign.getResults().getOrDefault("min", ""));
-        assert loopDesign.getResults().getOrDefault("min", "").trim().equals("" + mult_min) : "Failed to find minimum ! \n" + loopDesign.getResults().get("min");
+        assert loopDesign.getResults().getOrDefault("min.Plot1D", "").trim().equals("" + mult_min) : "Failed to find minimum ! \n" + loopDesign.getResults().get("min");
     }
 
     @Test
